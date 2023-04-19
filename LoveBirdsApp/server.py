d57f28d02fce51db11f76ae986ddc122d82ebe93
Danny Nguyen <nguyenduongpa@gmail.com>
2023-04-19 08:51:12 -0700

    lovebirds app added

---
+++ LoveBirdsApp/server.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from flask_app import app
+from flask_app.controllers import users
+
+if __name__=="__main__":
+    app.run(debug=True, host="localhost", port=8000)
