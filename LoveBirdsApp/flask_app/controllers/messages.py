ce962bbef4e898da395f27dac4a59aeb6834f4c3
Danny Nguyen <nguyenduongpa@gmail.com>
2023-06-06 08:53:31 -0700

    fixed up all css

--- LoveBirdsApp/flask_app/controllers/messages.py
+++ LoveBirdsApp/flask_app/controllers/messages.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash, url_for, Response
-from flask_app.models import user, message
+from flask_app.models import user, message, image
 
 @app.route('/messages/new/<int:id>')
 def new_message(id):
@@ -18,7 +18,20 @@ def new_message(id):
         'user_id': id
     }
     receiver = user.User.get_info_by_id(data)
-    return render_template('new_message.html', receiver=receiver)
+    data3 = {
+        'user_id': session['user_id']
+    }
+    myId = session['user_id']
+
+    all_pics = image.Photo.query.filter_by(user=myId).all()
+    user1 = user.User.get_info_by_id(data3)
+    if user1.suspended == "yes":
+        return redirect('/suspended')
+    for pic in all_pics:
+        if(pic.profile == "yes"):
+            profile_pic = pic
+            return render_template('new_message.html', profile=profile_pic, receiver=receiver, user=user1)
+    return render_template('new_message.html', receiver=receiver, user=user1)
 
 @app.route('/messages/process/<int:id>', methods=['POST'])
 def process_message(id):
@@ -27,8 +40,8 @@ def process_message(id):
     if not message.Message.validate_message(request.form):
         return redirect('/messages/new/' + str(id))
     message.Message.save_message(request.form)
-    
-    return redirect('/users/' + str(id))
+    myId = session['user_id']
+    return redirect('/users/' + str(myId))
 
 @app.route('/messages/delete/<int:id>')
 def delete_message(id):
